82c10c93845924c7c3141f4e605da57ed6c59748
h-tko <bojan_krkic_esp@i.softbank.jp>
2017-02-15 08:43:30 +0000

    add task_comments create

---
+++ db/migrations/20170215084107_create_task_comments.sql
@@ -0,0 +1,18 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE TABLE task_comments (
+    id serial not null primary key,
+    task_id integer not null,
+    memmber_id integer not null,
+    comment text not null,
+    created_at timestamp(0) not null default now(),
+    updated_at timestamp(0) not null default now(),
+    deleted_at timestamp(0)
+);
+
+CREATE INDEX task_comments_task_id_idx ON task_comments USING btree(task_id);
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE task_comments;
